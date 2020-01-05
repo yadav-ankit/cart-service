1255b9dcf328fe4daded871d7485320953d439a9
any65889@gmail.com <ankyadav@amdocs.com>
2020-01-05 09:58:13 +0530

    Port added

--- src/main/java/com/microservice/phonekart/cartservice/controller/MyCartController.java
+++ src/main/java/com/microservice/phonekart/cartservice/controller/MyCartController.java
@@ -2,24 +2,19 @@ package com.microservice.phonekart.cartservice.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MyCartController {
 
-	
-	@LocalServerPort
-	 int randomPort;
+	@Autowired
+	Environment  environment;
 	
 	@GetMapping(path="/mycart/getTotalItems")
 	public String getCartCount() {
 		
-
-		Integer port = randomPort; 
-		
-		String x= port.toString();
-		
-		return "any Number of items " + x;
+		return "any Number of items " + environment.getProperty("local.server.port");
 	}
 }
